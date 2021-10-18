683969f2ed06eb503f85bcc89431256fc9cd491a
Burak Yunkul <byunkul23@hotmail.com>
2021-10-19 00:20:40 +0300

    18 Ekim Javascript

---
+++ src/app.js
@@ -0,0 +1,26 @@
+console.log("Merhaba Kodlama.io")
+
+//Js not type safe
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    let dolarDun = 9.1
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+    //euroDun = 11 //bunu yapamıyoruz
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>" + konutKredileri[i] + "</li>")
+
+}
+console.log("</ul>")
+console.log(konutKredileri)
+
+console.log(euroDun)
